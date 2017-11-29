7224291f6b39b808c9ef379639f8fab63c0aaa6a
Rodrigo M <rodrigo.medina.neri@gmail.com>
2017-11-29 01:54:12 -0600

    Initial Commit

---
+++ src/components/Row.vue
@@ -0,0 +1,19 @@
+<template lang="html">
+  <tr>
+    <td>{{ specie.number }}</td>
+    <td>{{ specie.name }}</td>
+    <td><img :src="specie.ThumbnailImage" width="35"></td>
+  </tr>
+</template>
+
+<script>
+export default {
+
+  name:'Row',
+  props:['specie']
+
+}
+</script>
+
+<style lang="css">
+</style>
---
+++ src/views/Home/Home.vue
@@ -0,0 +1,53 @@
+<template lang="html">
+  <div>
+    <h1 class="home-view">Componente Listo</h1>
+    <table>
+      <thead>
+        <th>Id</th>
+        <th>Name</th>
+        <th>Image</th>
+      </thead>
+      <tbody>
+          <pokehtml v-for="specie in pokemon" :specie="specie"></pokehtml>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+
+import {getPokemon} from '@/services/pokemon'
+import Row from '@/components/Row'
+
+export default {
+
+  name:'HomeView',
+  components:{
+
+    pokehtml:Row
+
+  },
+  data (){
+    return {
+      pokemon:[]
+    }
+  },
+  created (){
+      getPokemon()
+      .then((response) => {
+
+        this.pokemon = response.data
+        console.log(response.data)
+
+      })
+      .catch(err => {
+
+          console.error(err);
+
+      })
+  }
+}
+</script>
+
+<style lang="css">
+</style>
